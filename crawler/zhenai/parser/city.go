2208fdcf7617fbbbcf256d430f3b74c3c242122a
akitaSummer <644171127@qq.com>
2021-02-01 23:45:28 +0800

    feature: add city parser

---
+++ crawler/zhenai/parser/city.go
@@ -0,0 +1,25 @@
+package parser
+
+import (
+	"learn_go/crawler/engine"
+	"regexp"
+)
+
+const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+
+func ParseCity(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityRe)
+	matches := re.FindAllSubmatch(contents, -1)
+	result := engine.ParseResult{}
+	for _, m := range matches {
+		name := string(m[2])
+		result.Items = append(result.Items, "User "+name)
+		result.Request = append(result.Request, engine.Request{
+			Url: string(m[1]),
+			ParserFunc: func(c []byte) engine.ParseResult {
+				return ParseProfile(c, name)
+			},
+		})
+	}
+	return result
+}
